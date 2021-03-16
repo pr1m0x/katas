80ee14fa0950ef594f936d7f2953a120eff9c58c
pr1m0x <kraemer.timo1@gmail.com>
2021-03-16 13:42:02 +0100

    added expected results comment

--- 7kyu/Highest and Lowest/index.js
+++ 7kyu/Highest and Lowest/index.js
@@ -10,3 +10,9 @@ function highAndLow(numbers) {
 
   return `${nums[0]} ${nums[nums.length - 1]}`;
 }
+
+/**
+ * highAndLow("1 2 3 4 5");  // return "5 1"
+ * highAndLow("1 2 -3 4 5"); // return "5 -3"
+ * highAndLow("1 9 3 4 -5"); // return "9 -5"
+ */
