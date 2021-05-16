5424473a0b8620dd5acfa7db2f15aeef52eb5a9c
pr1m0x <kraemer.timo1@gmail.com>
2021-05-16 10:14:36 +0200

    more zeros than ones kata

---
+++ 6kyu/more_zeros_than_ones/index.js
@@ -0,0 +1,20 @@
+// return only the letters that has more zeros than ones
+
+// remove duplicates
+// return null
+// return []
+
+function moreZeros(s) {
+  const arr = [];
+
+  [...new Set(s)].forEach((char) => {
+    if (
+      char.charCodeAt(0).toString(2).split(0).join('').length < char.charCodeAt(0).toString(2).split(1).join('').length
+    ) {
+      arr.push(char);
+    }
+  }, []);
+  return arr;
+}
+
+console.log(moreZeros('abcde'));
